b91bf429a28454305c1132799166a5600530230d
DianaSagoyan <dianasagoyan@yahoo.com>
2022-10-05 15:53:09 -0500

    Added methods to controllers

--- spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/CartController.java
+++ spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/CartController.java
@@ -1,8 +1,13 @@
 package com.cydeo.spring05thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CartController {
+    @GetMapping("/cart")
+    public String cartPage(){
+        return "/cart/show-cart";
+    }
 }
--- spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/ProductController.java
+++ spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/ProductController.java
@@ -1,20 +1,29 @@
 package com.cydeo.spring05thymeleaf.controller;
 
+import com.cydeo.spring05thymeleaf.model.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
 
-    @GetMapping("/list")
+    @GetMapping("/list" )
+    public String listPage(){
+        return "/product/list";
+    }
+
+    @PostMapping ("/create-product")
     public String createProductPage(){
         return "/product/list";
     }
 
     @GetMapping("/create-form")
     public String formPage(Model model){
+
+        model.addAttribute("product", new Product());
         return "/product/create-product";
     }
 
