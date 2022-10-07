90807e98821fb97c5e004d2f263ae44ac9303432
DianaSagoyan <dianasagoyan@yahoo.com>
2022-10-06 21:19:34 -0500

    Delete From cart done

--- spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/CartController.java
+++ spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/CartController.java
@@ -2,12 +2,20 @@ package com.cydeo.spring05thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
 
 @Controller
 public class CartController {
     @GetMapping("/cart")
     public String cartPage(){
         return "/cart/show-cart";
+    }
+
+    @GetMapping("/addToCart/{id}/{quantity}")
+    public String addingToCart(@PathVariable("id")UUID id, @PathVariable("quantity") Integer qantity){
+        return "/cart/showCart";
     }
 }
--- spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/service/impl/CartServiceImpl.java
+++ spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/service/impl/CartServiceImpl.java
@@ -2,12 +2,14 @@ package com.cydeo.spring05thymeleaf.service.impl;
 
 import com.cydeo.spring05thymeleaf.model.Cart;
 import com.cydeo.spring05thymeleaf.model.CartItem;
+import com.cydeo.spring05thymeleaf.model.Product;
 import com.cydeo.spring05thymeleaf.service.CartService;
 import com.cydeo.spring05thymeleaf.service.ProductService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -15,6 +17,7 @@ public class CartServiceImpl implements CartService {
     public static Cart CART = new Cart(BigDecimal.ZERO,new ArrayList<>());
 
     private final ProductService productService;
+    List<CartItem> cartItemList = new ArrayList<>();
 
     public CartServiceImpl(ProductService productService) {
         this.productService = productService;
@@ -23,19 +26,24 @@ public class CartServiceImpl implements CartService {
     @Override
     public Cart addToCart(UUID productId, Integer quantity){
         //todo retrieve product from repository method
-        //Check if product exists or not CartItem cartItem
         //todo initialise cart item
         //todo calculate cart total amount
         //todo add to cart
+
+        productService.findProductById(productId);
+        CartItem cartItem = new CartItem();
         return CART;
     }
 
     @Override
     public boolean deleteFromCart(UUID productId){
-        //todo delete product object from cart using stream
+        
+        BigDecimal deletingPrice = CART.getCartItemList().stream()
+                .filter(cartItem -> cartItem.getProduct().getId().toString().equals(productId.toString())).findAny().orElseThrow().getProduct().getPrice();
 
+        CART.setCartTotalAmount(CART.getCartTotalAmount().subtract(deletingPrice));
 
-        CART.getCartItemList().removeIf(cartItem -> cartItem.getProduct().getId().equals(productId));
+        CART.getCartItemList().removeIf(cartItem -> cartItem.getProduct().getId().toString().equals(productId.toString()));
 
         return true;
     }
