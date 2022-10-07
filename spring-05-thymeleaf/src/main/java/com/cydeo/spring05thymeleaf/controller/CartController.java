ebd5427ea612b9211ce1537dfd1fccfc5c988c92
DianaSagoyan <dianasagoyan@yahoo.com>
2022-10-07 14:51:17 -0500

    Working on Adding to Cart

--- spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/CartController.java
+++ spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/CartController.java
@@ -2,6 +2,7 @@ package com.cydeo.spring05thymeleaf.controller;
 
 import com.cydeo.spring05thymeleaf.model.Product;
 import com.cydeo.spring05thymeleaf.service.CartService;
+import com.cydeo.spring05thymeleaf.service.impl.CartServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,23 +15,25 @@ import java.util.UUID;
 @Controller
 public class CartController {
 
-    private final CartService cartService;
+    private final CartServiceImpl cartServiceImpl;
 
-    public CartController(CartService cartService) {
-        this.cartService = cartService;
+    public CartController(CartServiceImpl cartServiceImpl) {
+        this.cartServiceImpl = cartServiceImpl;
     }
 
     @GetMapping("/cart")
     public String cartPage(Model model){
 
+        model.addAttribute("cartItemList", cartServiceImpl.CART.getCartItemList());
+        model.addAttribute("cart", cartServiceImpl.CART);
+
         return "/cart/show-cart";
     }
 
-    @GetMapping("/addToCart/{id}/{quantity}")
-    public String addingToCart(@PathVariable("id")UUID id, @PathVariable("quantity") Integer quantity, @ModelAttribute("product") Product product, Model model){
+    @GetMapping("/addToCart")
+    public String addingToCart(@PathVariable("id")UUID id, @PathVariable("quantity") Integer quantity){
 
-        model.addAttribute("product", product);
-        cartService.addToCart(id, quantity);
+        cartServiceImpl.addToCart(id, quantity);
 
         return "/cart/show-cart";
     }
