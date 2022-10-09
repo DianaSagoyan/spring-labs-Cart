a97ec83d1c4ed7c4b9bf7ff934d36a4720c67573
DianaSagoyan <dianasagoyan@yahoo.com>
2022-10-09 14:28:52 -0500

    Updated template html files

--- spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/CartController.java
+++ spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/controller/CartController.java
@@ -1,14 +1,10 @@
 package com.cydeo.spring05thymeleaf.controller;
 
-import com.cydeo.spring05thymeleaf.model.Product;
-import com.cydeo.spring05thymeleaf.service.CartService;
 import com.cydeo.spring05thymeleaf.service.impl.CartServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.UUID;
 
